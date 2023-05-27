57ecc299071f34e64cc0bd0048f99221d45bd655
YiWenLW <125837326+YiWenLW@users.noreply.github.com>
2023-05-26 22:50:10 -0600

    Add files via upload
    Add stored procedures

---
+++ SP-lostupdate.sql
@@ -0,0 +1,67 @@
+﻿CREATE PROCEDURE [dbo].[ChangeContainerAmount]
+	@Amount INT,
+	@Container nchar(20)
+AS
+BEGIN
+
+	SET NOCOUNT ON -- no retorne metadatos
+
+	DECLARE @ErrorNumber INT, @ErrorSeverity INT, @ErrorState INT, @CustomError INT
+	DECLARE @Message VARCHAR(200)
+	DECLARE @InicieTransaccion BIT
+
+	-- declaracion de otras variables
+	DECLARE @Containerid INT
+	DECLARE @LocationBefore geography;
+	DECLARE @LocationAfter geography;
+
+	SET @LocationBefore = geography::Point(RAND() * 180 - 90, RAND() * 360 - 90, 4326);
+	SET @LocationAfter = geography::Point(RAND() * 180 - 90, RAND() * 360 - 90, 4326);
+
+	-- operaciones de select que no tengan que ser bloqueadas
+
+	SELECT @Containerid = containerid 
+	FROM Containers 
+	WHERE name = @Container;
+	
+	-- tratar de hacer todo lo posible antes de q inice la transaccion
+
+	SET @InicieTransaccion = 0
+	IF @@TRANCOUNT=0 BEGIN
+		SET @InicieTransaccion = 1
+		SET TRANSACTION ISOLATION LEVEL READ COMMITTED
+		BEGIN TRANSACTION
+	END
+
+	BEGIN TRY
+		SET @CustomError = 2001
+
+		-- En este caso, un ejemplo de cuando se genera el lost update es cuando ocurren dos transacciones al mismo tiempo sobre el mismo contenedor
+		-- El resultado será que una de las transacciones tendrá éxito y se insertará en la tabla "ContainerHistory"
+		-- Mientras que la otra transacción se encontrará con un error y realizará un rollback.
+
+		WAITFOR DELAY '00:00:05';
+
+		INSERT INTO ContainerHistory (containerid, date, movementType, quantity, locationBefore, locationAfter)
+		VALUES (@Containerid, GetDate(), 7, @Amount, @LocationBefore, @LocationAfter);
+
+
+		IF @InicieTransaccion=1 BEGIN
+			COMMIT
+		END
+	END TRY
+	BEGIN CATCH
+		SET @ErrorNumber = ERROR_NUMBER()
+		SET @ErrorSeverity = ERROR_SEVERITY()
+		SET @ErrorState = ERROR_STATE()
+		SET @Message = ERROR_MESSAGE()
+
+		IF @InicieTransaccion=1 BEGIN
+			ROLLBACK
+		END
+		RAISERROR('%s - Error Number: %i',
+			@ErrorSeverity, @ErrorState, @Message, @CustomError)
+	END CATCH
+END
+RETURN 0
+GO
---
+++ SP1-dirty-read.sql
@@ -0,0 +1,60 @@
+﻿CREATE PROCEDURE [dbo].[InsertProduct]
+	@Productid INT,
+	@Name nchar(20),
+	@Price decimal(12,2),
+	@Expenses decimal(12,2)
+
+AS
+BEGIN
+
+	SET NOCOUNT ON -- no retorne metadatos
+
+	DECLARE @ErrorNumber INT, @ErrorSeverity INT, @ErrorState INT, @CustomError INT
+	DECLARE @Message VARCHAR(200)
+	DECLARE @InicieTransaccion BIT
+
+	-- declaracion de otras variables
+	-- operaciones de select que no tengan que ser bloqueadas
+	
+	-- tratar de hacer todo lo posible antes de q inice la transaccion
+
+	SET @InicieTransaccion = 0
+	IF @@TRANCOUNT=0 BEGIN
+		SET @InicieTransaccion = 1
+		SET TRANSACTION ISOLATION LEVEL READ COMMITTED
+		BEGIN TRANSACTION
+	END
+
+	BEGIN TRY
+		SET @CustomError = 2001
+
+		-- Ese SP lo que hace es insertar un producto
+		-- En este caso se puede generar un dirty read se necesita los SP InsertProduct y AverageProductPrice
+		-- El dirty read se generaría en el caso donde se llame al InsertProduct y antes de que se haga el commit
+		-- Se llame al AverageProductPrice, lo que generaría un dirty read 
+		-- Esto ocurre porque la lectura del gasto promedio se realiza antes de que la transacción de inserción del producto se complete y se confirme
+		-- Lo que significa que se está leyendo un dato que aún no ha sido confirmado o puede ser revertido.
+
+		INSERT INTO Products (productid, name, price, expenses)
+		VALUES (@Productid, @Name, @Price, @Expenses);
+
+
+		IF @InicieTransaccion=1 BEGIN
+			COMMIT
+		END
+	END TRY
+	BEGIN CATCH
+		SET @ErrorNumber = ERROR_NUMBER()
+		SET @ErrorSeverity = ERROR_SEVERITY()
+		SET @ErrorState = ERROR_STATE()
+		SET @Message = ERROR_MESSAGE()
+
+		IF @InicieTransaccion=1 BEGIN
+			ROLLBACK
+		END
+		RAISERROR('%s - Error Number: %i',
+			@ErrorSeverity, @ErrorState, @Message, @CustomError)
+	END CATCH
+END
+RETURN 0
+GO
